141245c5aa64961904f6dc491a3f31a717f390ae
小游 <45655117+xiaoyou66@users.noreply.github.com>
2022-09-15 09:07:09 +0800

    update mysql

--- src/core/parse/mysql.ts
+++ src/core/parse/mysql.ts
@@ -1,16 +1,20 @@
 const tableTemplate = `CREATE TABLE IF NOT EXISTS <%-name%> (<%for(let field of fields){%>\n    <%-field%><%}%>
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4;
 `
 
 const generateCreate = (table:IDatabase) => {
     let fields:string[] = []
     // 字段生成
     table.fields.forEach((field:IDatabaseField) => {
-        fields.push(`\`${field.name}\` ${field.type.toUpperCase()}${field.notNull?" NOT NULL":""}${field.autoIncrease?" AUTO_INCREMENT":""} COMMENT '${field.desc}',`)
+        fields.push(`\`${field.name}\` ${field.type.toUpperCase()}${field.notNull||field.primary?" NOT NULL":""}${field.autoIncrease?" AUTO_INCREMENT":""} COMMENT '${field.desc}',`)
     })
     // 索引生成
     table.fields.forEach((field:IDatabaseField) => {
-
+        if (field.primary) {
+            fields.push(`PRIMARY KEY (\`${field.name}\`),`)
+        } else if (field.index) {
+            fields.push(`INDEX \`idx_${field.name}\`(\`${field.name}\`),`)
+        }
     })
     // @ts-ignore
     return ejs.render(tableTemplate, {
